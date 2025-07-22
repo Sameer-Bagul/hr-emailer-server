5d0c663e2b9a954e7cb230e49897d353a0032df4
sameer <sameerbagul2004@gmail.com>
2025-07-22 19:24:56 +0530

    server rearranged

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,7 +4,8 @@ const path = require('path');
 // Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    // Use absolute path relative to server root
+    cb(null, path.join(__dirname, '../../uploads'));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
